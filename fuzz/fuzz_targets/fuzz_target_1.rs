70bc9048f042345c8da51fb42a7a7cf7f97ac99d
Brian Anderson <andersrb@gmail.com>
2023-11-01 00:29:01 +0000

    Restructure time advancement

--- fuzz/fuzz_targets/fuzz_target_1.rs
+++ fuzz/fuzz_targets/fuzz_target_1.rs
@@ -11,9 +11,11 @@ use soroban_sdk::testutils::{
     MockAuthInvoke,
 };
 use soroban_sdk::{
+    Bytes,
     token::{Client, StellarAssetClient},
     Address, Env, FromVal, IntoVal, String,
 };
+use std::vec::Vec as RustVec;
 
 pub(crate) const DAY_IN_LEDGERS: u32 = 17280;
 pub(crate) const INSTANCE_BUMP_AMOUNT: u32 = 7 * DAY_IN_LEDGERS;
@@ -22,7 +24,7 @@ pub(crate) const INSTANCE_LIFETIME_THRESHOLD: u32 = INSTANCE_BUMP_AMOUNT - DAY_I
 #[derive(Clone, Debug, arbitrary::Arbitrary)]
 pub struct Input {
     addresses: [<Address as SorobanArbitrary>::Prototype; 3],
-    commands: Vec<Command>,
+    commands: RustVec<Command>,
 }
 
 #[derive(Clone, Debug, arbitrary::Arbitrary)]
@@ -34,7 +36,7 @@ pub enum Command {
     BurnFrom(#[arbitrary(with = |u: &mut Unstructured| u.int_in_range(0..=i128::MAX))] i128),
     Burn(#[arbitrary(with = |u: &mut Unstructured| u.int_in_range(0..=i128::MAX))] i128),
     // todo: adjust the range
-    AdvanceTimeToLedger(
+    AdvanceLedgers(
         #[arbitrary(with = |u: &mut Unstructured| u.int_in_range(0..=DAY_IN_LEDGERS))] u32,
     ),
 }
@@ -48,26 +50,40 @@ pub struct ApproveInput {
 }
 
 fuzz_target!(|input: Input| -> Corpus {
-    let prev_env = Config::setup();
-    let mut env = reset_env(&prev_env);
+    let mut env = create_env();
 
-    let admin = Address::from_val(&env, &input.addresses[0]);
-    let spender = Address::from_val(&env, &input.addresses[1]);
-    let to = Address::from_val(&env, &input.addresses[2]);
+    let token_contract_id_bytes: RustVec<u8>;
 
-    if !require_unique_addresses(&[&admin, &spender, &to]) {
-        return Corpus::Reject;
+    // Do initial setup, including registering the contract.
+    {
+        let admin = Address::from_val(&env, &input.addresses[0]);
+        let spender = Address::from_val(&env, &input.addresses[1]);
+        let to = Address::from_val(&env, &input.addresses[2]);
+
+        if !require_unique_addresses(&[&admin, &spender, &to]) {
+            return Corpus::Reject;
+        }
+
+        let token_contract_id_string = env.register_stellar_asset_contract(admin.clone()).to_string();
+        let mut token_contract_id_buf = vec![0; token_contract_id_string.len() as usize];
+        token_contract_id_string.copy_into_slice(&mut token_contract_id_buf);
+        token_contract_id_bytes = token_contract_id_buf
     }
-
-    let token_contract_id = env.register_stellar_asset_contract(admin.clone());
-    let admin_client = StellarAssetClient::new(&env, &token_contract_id);
-    let token_client = Client::new(&env, &token_contract_id);
 
     let mut contract_state = ContractState::init();
 
     println!("commands: {:#?}", input.commands);
     for command in input.commands {
-        env = reset_env(&env);
+        // The Env may be different for each step, so we need to reconstruct
+        // everything that depends on it.
+
+        let admin = Address::from_val(&env, &input.addresses[0]);
+        let spender = Address::from_val(&env, &input.addresses[1]);
+        let to = Address::from_val(&env, &input.addresses[2]);
+
+        let token_contract_id = Address::from_string_bytes(&Bytes::from_slice(&env, &token_contract_id_bytes));
+        let admin_client = StellarAssetClient::new(&env, &token_contract_id);
+        let token_client = Client::new(&env, &token_contract_id);
 
         match command {
             Command::Mint(amount) => {
@@ -203,30 +219,24 @@ fuzz_target!(|input: Input| -> Corpus {
                 }
                 assert_eq!(contract_state.admin_balance, token_client.balance(&admin));
             }
-            Command::AdvanceTimeToLedger(time) => {
-                if time > env.ledger().sequence() {
-                    advance_time_to(&env, &token_client, time);
-                    println!(
-                        "-- ledger after advance_time_to: {}",
-                        env.ledger().sequence()
-                    );
-                    println!(
-                        "-- contract_state.expiration_ledger: {}",
-                        contract_state.expiration_ledger
-                    );
-                    println!("-        state allowance: {}", contract_state.allowance);
-                    println!(
-                        "- token client allowance: {}",
-                        token_client.allowance(&admin, &spender)
-                    );
-
-                    if env.ledger().sequence() > contract_state.expiration_ledger {
-                        contract_state.allowance = 0;
-                    }
+            Command::AdvanceLedgers(ledgers) => {
+                let next_ledger = env.ledger().sequence().checked_add(ledgers).expect("end of time");
+                env = advance_time_to(env, &token_contract_id_bytes, next_ledger);
+                // NB: This env is reconstructed and all previous env-based objects are invalid
+                println!(
+                    "-- ledger after advance_time_to: {}",
+                    env.ledger().sequence()
+                );
+                println!(
+                    "-- contract_state.expiration_ledger: {}",
+                    contract_state.expiration_ledger
+                );
+                println!("-        state allowance: {}", contract_state.allowance);
+                if env.ledger().sequence() > contract_state.expiration_ledger {
+                    contract_state.allowance = 0;
                 }
             }
         }
-        env = reset_env(&env);
     }
 
     Corpus::Keep
@@ -252,43 +262,6 @@ impl ContractState {
     }
 }
 
-struct Config {}
-
-impl Config {
-    fn setup() -> Env {
-        let snapshot = {
-            let init_ledger = LedgerInfo {
-                protocol_version: 1,
-                sequence_number: 10,
-                timestamp: 12345,
-                network_id: Default::default(),
-                base_reserve: 10,
-                min_temp_entry_ttl: u32::MAX,
-                min_persistent_entry_ttl: u32::MAX,
-                max_entry_ttl: u32::MAX,
-            };
-
-            LedgerSnapshot::from(init_ledger, None)
-        };
-
-        let env = Env::from_snapshot(snapshot);
-        env.mock_all_auths();
-
-        env
-    }
-}
-
-fn reset_env(prev_env: &Env) -> Env {
-    let mut snapshot = prev_env.to_snapshot();
-    snapshot.sequence_number += 1;
-    //    snapshot.timestamp = snapshot.timestamp.saturating_add(advance_time);
-
-    let env = Env::from_snapshot(snapshot);
-    env.budget().reset_unlimited();
-
-    env
-}
-
 fn require_unique_addresses(addrs: &[&Address]) -> bool {
     for addr1 in addrs {
         let count = addrs.iter().filter(|a| a == &addr1).count();
@@ -299,24 +272,60 @@ fn require_unique_addresses(addrs: &[&Address]) -> bool {
     true
 }
 
-fn advance_time_to(env: &Env, token_client: &Client, to_ledger: u32) {
-    loop {
-        let next_ledger = env
-            .ledger()
-            .get()
-            .sequence_number
-            .saturating_add(DAY_IN_LEDGERS);
-        let next_ledger = next_ledger.min(to_ledger);
-        env.ledger().with_mut(|li| {
-            li.sequence_number = next_ledger;
-        });
-        env.budget().reset_default();
+fn create_env() -> Env {
+    Env::default()
+}
 
-        // Keep the contract alive
-        let _ = token_client.try_allowance(&Address::random(env), &Address::random(env));
+fn advance_env(prev_env: Env, ledgers: u32) -> Env {
+    use soroban_sdk::testutils::Ledger as _;
+
+    let secs_per_ledger = {
+        let secs_per_day = 60 * 60 * 24;
+        let ledgers_per_day = DAY_IN_LEDGERS as u64;
+        secs_per_day / ledgers_per_day
+    };
+    let ledger_time = secs_per_ledger.checked_mul(ledgers as u64).expect("end of time");
+
+    let mut env = prev_env.clone();
+    env.ledger().with_mut(|ledger| {
+        ledger.sequence_number = ledger.sequence_number.checked_add(ledgers).expect("end of time");
+        ledger.timestamp = ledger.timestamp.checked_add(ledger_time).expect("end of time");
+    });
+
+    env
+
+    /*
+    let mut snapshot = prev_env.to_snapshot();
+    snapshot.sequence_number = snapshot.sequence_number.checked_add(ledgers).expect("end of time");
+    snapshot.timestamp = snapshot.timestamp.checked_add(ledger_time).expect("end of time");
+
+    let env = Env::from_snapshot(snapshot);
+
+    env*/
+}
+
+/// Advance time, but do it in increments, periodically pinging the controct to
+/// keep it alive.
+fn advance_time_to(mut env: Env, token_contract_id_bytes: &[u8], to_ledger: u32) -> Env {
+    loop {
+        let curr_ledger = env.ledger().get().sequence_number;
+        assert!(curr_ledger < to_ledger);
+
+        let next_ledger = curr_ledger.checked_add(DAY_IN_LEDGERS).expect("end of time");
+        let next_ledger = next_ledger.min(to_ledger);
+        let advance_ledgers = next_ledger - curr_ledger;
+
+        env = advance_env(env, advance_ledgers);
 
         if next_ledger == to_ledger {
             break;
+        } else {
+            // Keep the contract alive
+            let token_contract_id = Address::from_string_bytes(&Bytes::from_slice(&env, &token_contract_id_bytes));
+            let token_client = Client::new(&env, &token_contract_id);
+            let _ = token_client.try_allowance(&Address::random(&env), &Address::random(&env));
         }
     }
+
+    env
 }
